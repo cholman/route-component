c792a2fda7f72ba5420c34c2d300b6b9e046edb7
cholman <holman.cody@gmail.com>
2019-12-23 12:12:53 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+
+function Home(props) {
+  return <h3>This is Home</h3>
+}
+function Blog(props){
+  const { push } = props.history;
+  return <>
+          <h3>My blog noone reads</h3> 
+          <button onClick={() => push("/")}>back home</button>
+        </>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Router>
+         <Link to='/'>Home</Link>
+         <Link to='/blog'>Blog</Link>
+
+         <Route exact path='/' component={Home} />
+         <Route path='/blog' component={Blog} />
+       </Router>
       </header>
     </div>
   );
